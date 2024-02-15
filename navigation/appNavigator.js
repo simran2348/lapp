d551cb8209d9cc9e1310ce08719043a6b7b6cc47
simranjit singh <simranjits008@gmail.com>
2024-02-15 21:26:53 +0530

    added app navigator

---
+++ navigation/appNavigator.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const appNavigator = () => {
+  return (
+    <View>
+      <Text>appNavigator</Text>
+    </View>
+  );
+};
+
+export default appNavigator;
+
+const styles = StyleSheet.create({});
